eab8c149ff5112f85f8460f6941a22b1e23edd9c
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-12-02 09:37:49 -0800

    sd

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("0")
     add = 0   
     for i in range(1, len(sys.argv)):
        add += int(sys.argv[i])
