884d23e734636c113cc8ee2451913ddead2c70ae
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-12-03 23:05:07 -0800

    sd

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list
     for i in range(0, len(my_list)):
         if idx < 0:
             return my_list
-        elif idx >= 0:
+        elif idx >= len(my_list):
             return my_list
-        if i == idx:
-            new_list[i] = element
-            return new_list
+        new_list = list(my_list)
+        new_list[idx] = element
+        return new_list
