565997e507355c70a3a9c2f15ca280332619f142
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-12-05 11:10:15 -0800

    sd

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,9 @@ def multiple_returns(sentence):
     for i in range(0, len(sentence)):
         if i == 0:
             first_char = sentence[i]
-        i = i + 1 
-    return i, first_char    
+        i = i + 1
+    if sentence == None:
+        first_char = None
+        return i, first_char
+    else:
+        return i, first_char    
