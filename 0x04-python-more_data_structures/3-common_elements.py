4e0b995b4797f20ca8433084e4bb5679d5e68700
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-12-08 04:46:11 -0800

    sd

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    my_list = []
-    for i in list(set_1):
-        for j in list(set_2):
-            if i == j:
-                my_list = j
-            else:
-                pass
-    return my_list        
+    return set_1 & set_2
