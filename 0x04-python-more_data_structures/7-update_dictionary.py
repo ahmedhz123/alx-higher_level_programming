c64862cebbfd671034831e9f208e65d70317055f
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-12-08 07:04:02 -0800

    sd

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    for i in a_dictionary.keys():
-        if i == key:
-            a_dictionary[i] = value
-    if key != i:       
-        a_dictionary[key] = value
-    return a_dictionary        
+my_dict[key] = value
+    return my_dict
