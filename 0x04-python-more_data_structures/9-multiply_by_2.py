57cdf083697659cde528c11aadd059c0cf854d71
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2023-12-08 10:53:08 -0800

    sd

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(my_dict):
     my_new_dict = my_dict.copy()
     for k in sorted(my_new_dict.keys()):
-        my_new_dict[k]= my_new_dict[k] * 2
+        my_new_dict[k] = my_new_dict[k] * 2
     return my_new_dict    
